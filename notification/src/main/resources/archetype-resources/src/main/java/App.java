775171b9d1b53d3174fc8a808aaf11b56342831f
erfan <erfanmorsalidev@gmail.com>
2025-10-23 13:12:46 +0330

    Feat : create new modules and add dependency for spring web

---
+++ notification/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.splitwise-simulator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
