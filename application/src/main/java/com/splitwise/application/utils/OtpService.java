3477579622e08c09a6591e4e4e8f90b81351a60b
erfan <erfanmorsalidev@gmail.com>
2025-10-23 14:38:25 +0330

    Feat : create OtpService Without Logic

---
+++ application/src/main/java/com/splitwise/application/utils/OtpService.java
@@ -0,0 +1,28 @@
+package com.splitwise.application.utils;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class OtpService {
+
+    public OtpResponse generateOtp(String key) {
+        return null;
+    }
+
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    public class OtpResponse {
+
+        private boolean sendOtp;
+        private String code;
+    }
+
+}
