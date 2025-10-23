eb4cae7b99ff3cad14afa132d9ed1636f209cb18
erfan <erfanmorsalidev@gmail.com>
2025-10-23 19:38:58 +0330

    Feat : Create Test Class Of GroupService

---
+++ application/src/test/java/com/splitwise/application/services/group/GroupServiceImplTest.java
@@ -0,0 +1,19 @@
+package com.splitwise.application.services.group;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+@ExtendWith(MockitoExtension.class)
+class GroupServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
