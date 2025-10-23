3be89dc711f5ae9c78258af132bae160fcdb8c97
erfan <erfanmorsalidev@gmail.com>
2025-10-23 16:38:09 +0330

    Feat : Create AdviceConfig To Handle Exceptions

---
+++ application/src/main/java/com/splitwise/application/configs/AdviceConfig.java
@@ -0,0 +1,40 @@
+package com.splitwise.application.configs;
+
+import com.splitwise.shared.objects.ErrorCodes;
+import com.splitwise.shared.objects.ErrorResult;
+import com.splitwise.shared.objects.StausCodes;
+import com.splitwise.shared.objects.SystemException;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.*;
+
+
+@RestControllerAdvice
+public class AdviceConfig {
+
+    @ExceptionHandler(SystemException.class)
+    public List<ErrorResult> handleSystemException(SystemException exception, HttpServletResponse response) {
+        response.setStatus(exception.getStatusCode().getCode());
+        return Collections.singletonList(new ErrorResult(exception));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public List<ErrorResult> handleValidationExceptions(MethodArgumentNotValidException ex, HttpServletResponse response) {
+        List<ErrorResult> validations = new ArrayList<>();
+
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            Map<String, Object> data = new HashMap<>();
+
+            data.put("fieldName", ((FieldError) error).getField());
+            data.put("errorMessage", error.getDefaultMessage());
+            validations.add(new ErrorResult(StausCodes.VALIDATION_EXCEPTION, ErrorCodes.VALIDATION_EXCEPTION.getCode(), data));
+        });
+
+        response.setStatus(StausCodes.VALIDATION_EXCEPTION.getCode());
+        return validations;
+    }
+}
--- shared/src/main/java/com/splitwise/shared/objects/ErrorCodes.java
+++ shared/src/main/java/com/splitwise/shared/objects/ErrorCodes.java
@@ -6,12 +6,15 @@ import lombok.Getter;
 @Getter
 public enum ErrorCodes {
     // User
-    USER_NOT_FOUND(1, "User not found"),
+    USER_NOT_FOUND(1, "User Not Found"),
 
 
     // Otp
-    INVALID_OTP(20, "Invalid OTP");
+    INVALID_OTP(20, "Invalid OTP"),
 
+
+    // General
+    VALIDATION_EXCEPTION(5050, "Validation Exception"),;
 
     private final Integer code;
     private final String message;
