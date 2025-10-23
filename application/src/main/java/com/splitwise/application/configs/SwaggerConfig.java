c8c8863b788652661e527bf39a9ff5ff02f87afe
erfan <erfanmorsalidev@gmail.com>
2025-10-23 19:12:12 +0330

    Feat : Create SecurityConfig And Change SwaggerConfig To Support JWt Authentication

--- application/src/main/java/com/splitwise/application/configs/SwaggerConfig.java
+++ application/src/main/java/com/splitwise/application/configs/SwaggerConfig.java
@@ -1,12 +1,22 @@
 package com.splitwise.application.configs;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 
-
+@SecurityScheme(
+        name = "Authorization",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        scheme = "Bearer",
+        in = SecuritySchemeIn.HEADER
+)
 @Configuration
 public class SwaggerConfig {
 
@@ -16,8 +26,9 @@ public class SwaggerConfig {
                 .group("All APIs")
                 .pathsToMatch("/**")
                 .addOpenApiCustomizer(openApi -> openApi.info(new Info()
-                        .title("swagger")
-                        .description("list of all APIs"))
+                                .title("swagger")
+                                .description("list of all APIs"))
+                        .addSecurityItem(new SecurityRequirement().addList("Authorization"))
                 ).build();
     }
 }
