32faec89f6873fb7870eb8aa6d4c7eb59d15f28f
erfan <erfanmorsalidev@gmail.com>
2025-10-23 18:32:32 +0330

    Feat : Remove Some Classes From notification Module And Create NotificationApplication.java Class

---
+++ notification/src/main/java/com/splitwise/notification/NotificationApplication.java
@@ -0,0 +1,14 @@
+package com.splitwise.notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+public class NotificationApplication {
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        SpringApplication.run(NotificationApplication.class, args);
+    }
+}
