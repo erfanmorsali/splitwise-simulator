ccc6c9e547cbe712de23a76122c0431a0a99c9d2
erfan <erfanmorsalidev@gmail.com>
2025-10-23 16:51:46 +0330

    Feat : Implement verifyOtp In AuthServiceImpl

--- application/src/main/java/com/splitwise/application/services/auth/AuthServiceImpl.java
+++ application/src/main/java/com/splitwise/application/services/auth/AuthServiceImpl.java
@@ -6,9 +6,12 @@ import com.splitwise.application.models.dtos.auth.VerifyOtpRequest;
 import com.splitwise.application.models.entities.user.UserEntity;
 import com.splitwise.application.services.user.UserService;
 import com.splitwise.application.utils.OtpService;
-import jakarta.transaction.Transactional;
+import com.splitwise.shared.objects.ErrorCodes;
+import com.splitwise.shared.objects.StausCodes;
+import com.splitwise.shared.objects.SystemException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -20,7 +23,7 @@ public class AuthServiceImpl implements AuthService {
     private final OtpService otpService;
 
     @Override
-    @Transactional(rollbackOn = Exception.class)
+    @Transactional(rollbackFor = Exception.class)
     public Boolean requestOtp(OtpRequest request) {
         String mobile = request.getMobile();
 
@@ -44,8 +47,17 @@ public class AuthServiceImpl implements AuthService {
         return true;
     }
 
+    @Transactional(readOnly = true)
     public TokenResponse verifyOtp(VerifyOtpRequest request) {
-        // implement Logic
+        UserEntity user = userService.findByMobile(request.getMobile())
+                .orElseThrow(() -> new SystemException(StausCodes.BAD_REQUEST, ErrorCodes.USER_NOT_FOUND, "Invalid mobile"));
+
+        boolean isValid = otpService.validateOtp(user.getMobile(), request.getMobile());
+        if (!isValid) {
+            throw new SystemException(StausCodes.BAD_REQUEST, ErrorCodes.INVALID_OTP, "Invalid OTP");
+        }
+
+        // generate Jwt Token
         return new TokenResponse();
     }
 }
