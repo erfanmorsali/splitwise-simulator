7cb662cff55096016f25dbc43c4b6db02b1810da
erfan <erfanmorsalidev@gmail.com>
2025-10-24 14:30:59 +0330

    Feat : Add  @EnableScheduling To Run Out Scheduled Job

--- application/src/main/java/com/splitwise/application/Api.java
+++ application/src/main/java/com/splitwise/application/Api.java
@@ -4,10 +4,12 @@ package com.splitwise.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 @EnableCaching
+@EnableScheduling
 @SpringBootApplication
 public class Api {
     public static void main(String[] args) {
